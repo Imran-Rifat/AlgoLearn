e48480c524d7e70e5f2ad51f5c14e412735c160d
imran tahmid <athormod2@gmail.com>
2025-12-01 14:53:23 -0800

    fix: pytest.ini BOM, test structure for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-﻿import pytest
+import pytest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from main import app
+from backend.main import app
 
 @pytest.fixture
 def client():
